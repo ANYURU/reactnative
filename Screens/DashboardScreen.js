bdc11952da925269d9d710e03871badd13965cdb
David Derrick Anyuru <davidderrickanyuru@gmail.com>
2022-03-02 07:50:24 +0300

    Dashboard Font

--- Screens/DashboardScreen.js
+++ Screens/DashboardScreen.js
@@ -6,40 +6,43 @@ const DashboardScreen = ({navigation}) => {
   return (
     <View style={GLOBAL_STYLES.dashboard}>
       {/* <Text>Dashboard</Text> */}
-      <TouchableOpacity 
-        style={[GLOBAL_STYLES.dashboardItem, GLOBAL_STYLES.dashboardItemPurple]} 
-        onPress={()=>{
+        <TouchableOpacity 
+            style={[GLOBAL_STYLES.dashboardItem, GLOBAL_STYLES.dashboardItemPurple]} 
+            onPress={()=>{
             navigation.navigate('Prayer')
-      }}
-      >
-        <Text 
-            style={
-                [
-                    GLOBAL_STYLES.dashboardItemText, 
-                    GLOBAL_STYLES.dashboardFont
-                ]
-            }
+            }}
+            >
+            <Text 
+                style={[
+                        GLOBAL_STYLES.dashboardItemText, 
+                        GLOBAL_STYLES.headings
+                ]}
             >Prayer Request
-        </Text>
-    </TouchableOpacity>
-      <TouchableOpacity style={[GLOBAL_STYLES.dashboardItem, GLOBAL_STYLES.dashboardItemOrange]} onPress={()=>{
-          navigation.navigate('Bible')
-      }}><Text style={[GLOBAL_STYLES.dashboardItemText, GLOBAL_STYLES.headings]}>Bible Study</Text></TouchableOpacity>
-      <TouchableOpacity style={[GLOBAL_STYLES.dashboardItem, GLOBAL_STYLES.dashboardItemWhite]} onPress={()=>{
+            </Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={[GLOBAL_STYLES.dashboardItem, GLOBAL_STYLES.dashboardItemOrange]} onPress={()=>{
+            navigation.navigate('Bible')
+        }}>
+            <Text style={[
+                GLOBAL_STYLES.dashboardItemText, 
+                GLOBAL_STYLES.headings
+                ]}>Bible Study</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={[GLOBAL_STYLES.dashboardItem, GLOBAL_STYLES.dashboardItemWhite]} onPress={()=>{
           navigation.navigate('Testimonies')
-      }}><Text style={GLOBAL_STYLES.dashboardItemText}>Testimonies</Text></TouchableOpacity>
+      }}><Text style={[GLOBAL_STYLES.dashboardItemText, GLOBAL_STYLES.headings]}>Testimonies</Text></TouchableOpacity>
       <TouchableOpacity style={[GLOBAL_STYLES.dashboardItem, GLOBAL_STYLES.dashboardItemGreen]} onPress={()=>{
           navigation.navigate('Service')
-      }}><Text style={GLOBAL_STYLES.dashboardItemText}>Main Service</Text></TouchableOpacity>
+      }}><Text style={[GLOBAL_STYLES.dashboardItemText, GLOBAL_STYLES.headings]}>Main Service</Text></TouchableOpacity>
       <TouchableOpacity style={[GLOBAL_STYLES.dashboardItem, GLOBAL_STYLES.dashboardItemPink]} onPress={()=>{
           navigation.navigate('Cell')
-      }}><Text style={GLOBAL_STYLES.dashboardItemText}>Online Cell</Text></TouchableOpacity>
+      }}><Text style={[GLOBAL_STYLES.dashboardItemText, GLOBAL_STYLES.headings]}>Online Cell</Text></TouchableOpacity>
       <TouchableOpacity style={[GLOBAL_STYLES.dashboardItem, GLOBAL_STYLES.dashboardItemPurple]} onPress={()=>{
           navigation.navigate('Tithe')
-      }}><Text style={GLOBAL_STYLES.dashboardItemText}>Tithe</Text></TouchableOpacity>
+      }}><Text style={[GLOBAL_STYLES.dashboardItemText, GLOBAL_STYLES.headings]}>Tithe</Text></TouchableOpacity>
       <TouchableOpacity style={[GLOBAL_STYLES.dashboardItem, GLOBAL_STYLES.dashboardItemOrange]} ronPress={()=>{
           navigation.navigate('Donate')
-      }}><Text style={GLOBAL_STYLES.dashboardItemText}>Donate</Text></TouchableOpacity>
+      }}><Text style={[GLOBAL_STYLES.dashboardItemText, GLOBAL_STYLES.headings]}>Donate</Text></TouchableOpacity>
     </View>
   )
 }
