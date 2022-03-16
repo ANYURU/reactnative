6e4e9db8a148855d3a6cace4d83875fb8d88afbf
David Derrick Anyuru <davidderrickanyuru@gmail.com>
2022-03-16 11:14:52 +0300

    Main service: redesign

--- Screens/ServiceScreen.js
+++ Screens/ServiceScreen.js
@@ -1,10 +1,30 @@
-import { View, Text } from 'react-native'
+import { View, Button, Text} from 'react-native'
 import React from 'react'
+import YoutubePlayer from 'react-native-youtube-iframe'
+import mainService from '../helpers/main-service'
 
 const ServiceScreen = () => {
+  const [{title, preacher, videoId, date}] = mainService
+
+  const [playing, setPlaying] = React.useState(false)
+  const togglePlaying = () => {
+    setPlaying((prev) => !prev)
+  }
+
   return (
     <View>
-      <Text>ServiceScreen</Text>
+      <Text>{title}</Text>
+      <YoutubePlayer
+        height={300}
+        play={playing}
+        videoId={videoId}
+      />
+      <Text>{`by ${preacher}`}</Text>
+      <Text>{date}</Text>
+      {/* <Button 
+        title={playing ? 'pause' : 'play'}
+        onPress={togglePlaying}
+      /> */}
     </View>
   )
 }
---
+++ helpers/main-service.js
@@ -0,0 +1,11 @@
+const DATA = [
+    {
+        id:1,
+        title:"Prayer and Fasting.",
+        date:"2021-01-05",
+        videoId:"84WIaK3bl_s",
+        preacher: "Pastor Komagum"
+    }
+]
+
+export default DATA
